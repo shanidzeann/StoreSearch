744733f67acfc87ee8609ee7d04330f71ed3b0cf
shanidzeann <shanidzeann@mail.ru>
2021-12-01 23:14:21 +0300

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,13 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Анна Шанидзе on 01.12.2021.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var searchResults = [SearchResult]()
+    
+    var hasSearched = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+}
 
 
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !hasSearched {
+            return 0
+        } else {
+            return searchResults.count == 0 ? 1 : searchResults.count
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "searchResultCell"
+        var cell: UITableViewCell! = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        }
+        
+        var content = cell.defaultContentConfiguration()
+        if searchResults.count == 0 {
+            content.text = "(Nothing found)"
+            content.secondaryText = ""
+        } else {
+            content.text = searchResults[indexPath.row].name
+            content.secondaryText = searchResults[indexPath.row].artistName
+        }
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return searchResults.count == 0 ? nil : indexPath
+    }
+    
+}
+
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        
+        searchResults = []
+        if searchBar.text! != "Bib" {
+            for i in 0...2 {
+                let searchResult = SearchResult()
+                searchResult.name = String(format: "Fake result %d for", i)
+                searchResult.artistName = searchBar.text!
+                searchResults.append(searchResult)
+            }
+        }
+        hasSearched = true
+        tableView.reloadData()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
 }
 
