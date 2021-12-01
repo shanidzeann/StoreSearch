e0cc780d0b8efd4b872dee008a908ae8144b2e1f
shanidzeann <shanidzeann@mail.ru>
2021-12-01 18:36:59 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Анна Шанидзе on 01.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
