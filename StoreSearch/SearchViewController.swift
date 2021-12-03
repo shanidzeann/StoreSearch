c57401975b5e589a28e0b20210ed6522a428c553
shanidzeann <shanidzeann@mail.ru>
2021-12-03 21:15:20 +0300

    Show keyboard on app launch

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -26,6 +26,8 @@ class SearchViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        searchBar.becomeFirstResponder()
+        
         var cellNib = UINib(nibName: TableView.CellIdentifiers.searchResultCell, bundle: nil)
         tableView.register(cellNib, forCellReuseIdentifier: TableView.CellIdentifiers.searchResultCell)
         
